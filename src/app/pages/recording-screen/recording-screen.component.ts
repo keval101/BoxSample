1d4b6e8e62d86ecd71f4c15a8b5afb22be9afea7
rushit.vora <rushit.vora@virtamed.com>
2021-08-11 17:25:33 +0530

    Video Controls implemented

--- src/app/pages/recording-screen/recording-screen.component.ts
+++ src/app/pages/recording-screen/recording-screen.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { RecordingService } from './recording.service';
 import { fadeAnimation } from '../../shared/app.animation';
+import { ChoosescreenshotService } from '../choose-screenshot/choosescreenshot.service';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recording-screen',
@@ -15,14 +17,41 @@ export class RecordingScreenComponent implements OnInit {
   isScreenShot: boolean = false;
   takeScreenshot: boolean = false;
   isSidebarOpen: boolean = false;
+  counterTime:boolean = false;
+  paddingClass:boolean;
+  data = [3,2,1,"go"]
+  text;
 
   constructor(
     public TranslateService: TranslateService,
     private router: Router,
-    private recordingService: RecordingService
+    private recordingService: RecordingService,
+    private choosescreenshotService : ChoosescreenshotService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const obs = interval(1000)
+    const timer:Subscription = obs.subscribe( (d) => {
+      this.counterTime = true;
+      let text = this.data[d];
+      this.text = text
+
+      if(d == 3){
+        this.paddingClass = true
+      }
+    })
+    setTimeout(() => {
+        timer.unsubscribe()
+        this.counterTime = false
+    }, 5000 );
+
+    if(this.choosescreenshotService.backToScreen == true){
+      timer.unsubscribe()
+      this.takeScreenshot = true;
+      this.recording = true;
+      this.isScreenShot = true;
+    }
+  }
 
   onRetake() {
     this.takeScreenshot = false;
--- src/app/pages/videoscreen/videoscreen.component.ts
+++ src/app/pages/videoscreen/videoscreen.component.ts
@@ -9,7 +9,7 @@ import { fadeAnimation } from '../../shared/app.animation';
   styleUrls: ['./videoscreen.component.scss'],
   animations: [fadeAnimation],
 })
-export class VideoscreenComponent implements OnInit{
+export class VideoscreenComponent implements OnInit, AfterViewInit{
   recording: boolean;
   isScreenShot: boolean;
   isVideoScreen:boolean
@@ -38,6 +38,59 @@ export class VideoscreenComponent implements OnInit{
     this.width = window.innerWidth
   };
 
+  togglePlayPause(){
+    if(this.video.nativeElement.paused){
+      this.playVideo = true
+      this.video.nativeElement.play()
+    }
+    else{
+      this.playVideo = false
+      this.video.nativeElement.pause()
+    }
+  };
+
+  ngAfterViewInit() {
+    this.video.nativeElement.addEventListener('timeupdate', ()=>{
+      var progressBar = document.getElementById('progressBar')
+      var time = this.video.nativeElement.currentTime / this.video.nativeElement.duration;
+      progressBar.style.width = time * 100 + "%";
+      if(this.video.nativeElement.ended){
+        this.playVideo = false
+      }
+
+    document.getElementById('seekbar').addEventListener('click', (e) => {
+      const progressTime = (e.offsetX / this.width) *   this.video.nativeElement.duration
+      this.video.nativeElement.currentTime = progressTime;
+    })
+    } );
+  }
+
+  onPlayPause(){
+    this.togglePlayPause()
+   };
+   
+   onRestart(){
+    if(this.video.nativeElement.currentTime > 0 ){
+      if(this.playVideo === true){
+      this.video.nativeElement.currentTime = 0;
+        this.playVideo = true
+        this.video.nativeElement.play()
+      }else if(this.playVideo === false){
+      this.video.nativeElement.currentTime = 0;
+        this.playVideo = false
+        this.video.nativeElement.pause()
+      }
+
+    } 
+   }
+
+   volumeChanged(e){
+    if (e.cancelable) {
+      e.preventDefault();
+   }
+     this.val = e
+     this.video.nativeElement.volume = this.val / 100
+   }
 
    redirectToBack(){
      this.router.navigate(['/intro'])
