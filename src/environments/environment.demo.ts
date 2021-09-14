ae4df059f427f3ccbe4c6a8a2225a4282992cdde
Fabian <fabian@uxy.ch>
2021-09-14 10:41:22 +0200

    adapted environments

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -1,11 +1,4 @@
-import { Environment } from './models/environment.interface';
-
-/**
- * // The file contents for the current environment will overwrite these during build.
- * // The build system defaults to the dev environment which uses `environment.ts`, but if you do
- * // `ng build --env=prod` then `environment.prod.ts` will be used instead.
- * // The list of which env maps to which file can be found in `.angular-cli.json`.
- */
+import { Environment } from './interfaces/environment.interface';
 
 export const environment: Environment = {
   production: true,
