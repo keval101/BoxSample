a8e7f08cbe3f2b5e7e81a253cb79c94309dbe9f9
Rushit Vora <rushit.vora@virtamed.com>
2021-12-07 15:51:25 +0530

    cancel exercise bug fixes

--- src/app/pages/end-screen/end-screen.component.ts
+++ src/app/pages/end-screen/end-screen.component.ts
@@ -34,8 +34,8 @@ export class EndScreenComponent implements OnInit {
         SceneNameLanguageKey: this.appData.nameLanguageKey,
         SceneSectionLanguageKey: this.appData.sceneSectionNameLanguageKey,
         ActualSceneNameLanguageKey: this.appData.nameLanguageKey,
-        StartDate: '',
-        EndDate: '',
+        StartDate: new Date().toISOString(),
+        EndDate: new Date().toISOString(),
         Status: 20,
         UserSession: uuidv4(),
         SimulatorIdentifier: 'cc43f6cb-d382-4783-a337-6532e3b2cdb1',
@@ -44,8 +44,6 @@ export class EndScreenComponent implements OnInit {
         Score: 0,
         MaxScore: 0,
         SelfAssesmentRating: 0,
-        TimeZoneString:
-          'W. Europe Standard Time;60;(UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna;W. Europe Standard Time;W. Europe Daylight Time;[01:01:0001;12:31:9999;60;[0;02:00:00;3;5;0;];[0;03:00:00;10;5;0;];];',
       };
 
       this.dataservice
